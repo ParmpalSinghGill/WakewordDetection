66d4228c65dde34dace499359b45d0c9bca6f5bc
Parmpal <query.aichunks@gmail.com>
2022-06-18 20:57:33 +0530

    Save And InferenceAudio

--- Inferences.py
+++ Inferences.py
@@ -1,8 +1,92 @@
+import time,datetime
 from tensorflow.keras.models import load_model
+# import logging
+import numpy as np
+import sounddevice,pyaudio
+from scipy.io.wavfile import write
+from Utils import getMFCC
+# logging.basicConfig(level=20)
 
-model=load_model("model.h5")
-checkpoint_filepath = 'Models/'
-model.load_weights(checkpoint_filepath)
-print(model.summary())
+# import python_speech_features
+# num_mfcc = 16
+# def getMFCC(data,samplerate):
+#     mfccs = python_speech_features.base.mfcc(data,samplerate=samplerate,
+#          winlen=0.256,winstep=0.050,numcep=num_mfcc,nfilt=26,
+#          nfft=2048,preemph=0.0,ceplifter=0,appendEnergy=False,winfunc=np.hanning)
+#     return mfccs
 
+
+samplerate=8000 # Record at 44100 samples per second
+chunk = 1024  # Record in chunks of 1024 samples
+sample_format = pyaudio.paInt16  # 16 bits per sample
+filename="Rec.wav"
+
+
+def SaveAudio():
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
+    print('Start recording')
+    stream = p.open(format=sample_format,
+                   channels=1,rate=samplerate,
+                   frames_per_buffer=chunk,input=True)
+    st,stt=time.time(),time.time()
+    frames = []
+    try:
+        while True:
+            data = stream.read(chunk)
+            frames.append(np.frombuffer(data, np.int16))
+    except KeyboardInterrupt as ke:
+        pass
+    # Stop and close the stream
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    print('Finished recording')
+    print(len(frames),frames[0].shape)
+    frames=np.concatenate(frames,axis=0)
+    print(frames.shape)
+    write(filename, samplerate, frames.astype(np.int16))
+
+model=load_model("h5models/modelTraind_85.h5")
+labels=['yes', 'no', 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go', 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'bed', 'bird', 'cat', 'dog', 'happy', 'house', 'marvin', 'sheila', 'tree', 'wow', 'backward', 'forward', 'follow', 'learn', 'visual', '_silence_']
+def predict(data):
+    mfcc=getMFCC(np.concatenate(data,axis=0),samplerate)
+    mfcc=np.reshape(mfcc,(1,*mfcc.shape,1))
+    pred=np.argmax(model.predict(mfcc)[0])
+    print(pred,labels[pred])
+
+def Inference(eachsecond=.5):
+    chunk=int(samplerate*eachsecond)
+    print("chunk",chunk)
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
+    print('Start recording')
+    stream = p.open(format=sample_format,
+                   channels=1,rate=samplerate,
+                   frames_per_buffer=chunk,input=True)
+    st,stt=time.time(),time.time()
+    frames,framepersonds = [],samplerate//chunk
+    print(framepersonds)
+    try:
+        while True:
+            data = stream.read(chunk)
+            frames.append(np.frombuffer(data, np.int16))
+            if len(frames)>framepersonds:
+                predict(frames[-framepersonds:])
+            # print(datetime.datetime.now())
+    except KeyboardInterrupt as ke:
+        pass
+    # Stop and close the stream
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    print('Finished recording')
+    print(len(frames),frames[0].shape)
+    frames=np.concatenate(frames,axis=0)
+    print(frames.shape)
+    write(filename, samplerate, frames.astype(np.int16))
+
+
+
+if __name__ == '__main__':
+    # SaveAudio()
+    Inference()
 
--- SaveAudioFile.py
+++ SaveAudioFile.py
@@ -1,7 +1,9 @@
+import time
+
 import sounddevice as sd
 from scipy.io.wavfile import write
 import pyaudio,wave
-
+import numpy as np
 
 def SaveAudio():
     fs = 44100  # Sample rate
@@ -11,13 +13,12 @@ def SaveAudio():
     sd.wait()  # Wait until recording is finished
     write('output.wav', fs, myrecording)  # Save as WAV file
 
-def saveAudioStremForSecond():
+def saveAudioStremForSecond(seconds=4):
 
     chunk = 1024  # Record in chunks of 1024 samples
     sample_format = pyaudio.paInt16  # 16 bits per sample
     channels = 2
     fs = 44100  # Record at 44100 samples per second
-    seconds = 3
     filename = "output.wav"
 
     p = pyaudio.PyAudio()  # Create an interface to PortAudio
@@ -54,16 +55,12 @@ def saveAudioStremForSecond():
 
 
 def saveAudioStrem():
-
     chunk = 1024  # Record in chunks of 1024 samples
     sample_format = pyaudio.paInt16  # 16 bits per sample
     channels = 2
     fs = 44100  # Record at 44100 samples per second
-    seconds = 3
     filename = "output.wav"
-
     p = pyaudio.PyAudio()  # Create an interface to PortAudio
-
     print('Recording')
 
     stream = p.open(format=sample_format,
@@ -97,5 +94,128 @@ def saveAudioStrem():
     wf.writeframes(b''.join(frames))
     wf.close()
 
+def saveAudioStremNpOneChannel(seconds=4):
+    chunk = 1024  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    channels = 1
+    fs = 44100  # Record at 44100 samples per second
+    filename = "output1.wav"
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
+    print('Recording')
+
+    stream = p.open(format=sample_format,
+                    channels=channels,
+                    rate=fs,
+                    frames_per_buffer=chunk,
+                    input=True)
+
+    frames = []  # Initialize array to store frames
+
+    # Store data in chunks for 3 seconds
+    try:
+        # while True:
+        for i in range(0, int(fs / chunk * seconds)):
+            data = stream.read(chunk)
+            frames.append(np.frombuffer(data, np.int16))
+    except KeyboardInterrupt as ke:
+        pass
+    # Stop and close the stream
+    stream.stop_stream()
+    stream.close()
+    # Terminate the PortAudio interface
+    p.terminate()
+
+    print('Finished recording')
+    print(len(frames),frames[0].shape)
+    frames=np.concatenate(frames,axis=0)
+    print(frames.shape)
+    write(filename, fs, frames.astype(np.int16))
+
+def saveAudioStremNp(seconds=4,channels = 2):
+    chunk = 1024  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    fs = 44100  # Record at 44100 samples per second
+    filename = "output1.wav"
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
+    print('Recording')
+
+    stream = p.open(format=sample_format,
+                    channels=channels,
+                    rate=fs,
+                    frames_per_buffer=chunk,
+                    input=True)
+
+    frames = []  # Initialize array to store frames
+
+    # Store data in chunks for 3 seconds
+    print(fs / chunk * seconds,fs , chunk , seconds)
+    try:
+        # while True:
+        for i in range(0, int(fs / chunk * seconds)):
+            data = stream.read(chunk)
+            if channels==1:
+                frames.append(np.frombuffer(data, np.int16))
+            else:
+                frames.append(np.frombuffer(data, np.int16).reshape(-1,2))
+    except KeyboardInterrupt as ke:
+        pass
+    # Stop and close the stream
+    stream.stop_stream()
+    stream.close()
+    # Terminate the PortAudio interface
+    p.terminate()
+
+    print('Finished recording')
+    print(len(frames),frames[0].shape)
+    frames=np.concatenate(frames,axis=0)
+    print(frames.shape)
+    write(filename, fs, frames.astype(np.int16))
+
+def saveAudioStremNpExperiment(seconds=4,channels = 1):
+    fs = 44100  # Record at 44100 samples per second
+    chunk = fs  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    filename = "output1.wav"
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
+    print('Recording')
+
+    stream = p.open(format=sample_format,
+                    channels=channels,
+                    rate=fs,
+                    frames_per_buffer=chunk,
+                    input=True)
+
+    frames = []  # Initialize array to store frames
+
+    # Store data in chunks for 3 seconds
+    try:
+        i=0
+        print(fs / chunk * seconds)
+        while True:
+            if seconds and i >(fs / chunk * seconds): break
+            i+=1
+            data = stream.read(chunk)
+            if channels==1:
+                frames.append(np.frombuffer(data, np.int16))
+            else:
+                frames.append(np.frombuffer(data, np.int16).reshape(-1,2))
+
+    except KeyboardInterrupt as ke:
+        pass
+    # Stop and close the stream
+    stream.stop_stream()
+    stream.close()
+    # Terminate the PortAudio interface
+    p.terminate()
+
+    print('Finished recording')
+    print(len(frames),frames[0].shape)
+    frames=np.concatenate(frames,axis=0)
+    print(frames.shape)
+    write(filename, fs, frames.astype(np.int16))
+
+
+
 # SaveAudio()
-saveAudioStrem()
+# saveAudioStremForSecond()
+saveAudioStremNpExperiment()
